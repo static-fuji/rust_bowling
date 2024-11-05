d303c007a391520a83c1935c8f0ed988746ae1fc
7K21qv <7K21qv@cyber-dojo.org>
2024-11-05 14:31:59 +0000

    8 ran tests, no prediction, got amber

--- files/src/lib.rs
+++ files/src/lib.rs
@@ -7,14 +7,11 @@ pub fn answer(score_str: &str) -> i32 {
 
     while frame_index < frames.len() && frame_index < 10 {
         let frame = frames[frame_index];
-        let first_roll = parse_roll(frame.chars().nth(0).unwrap());
-        let second_roll = parse_roll(frame.chars().nth(1).unwrap_or('0'));
+        let first_roll = frame.chars().nth(0).unwrap();
+        let second_roll = frame.chars().nth(1).unwrap_or('0');
 
         total_score += first_roll + second_roll;
 
-        if second_roll == 10 - first_roll {
-            total_score += bonus_for_spare(&frames, frame_index);
-        }
         frame_index += 1;
     }
     total_score
