6a56c231eccb28da03eabbe9fe8a0ff70ab70224
7K21qv <7K21qv@cyber-dojo.org>
2024-11-05 16:15:50 +0000

    43 ran tests, no prediction, got red

--- files/tests/lib_tests.rs
+++ files/tests/lib_tests.rs
@@ -10,4 +10,5 @@ fn life_the_universe_and_everything() {
     assert_eq!(31, hiker::answer("|1/|3/|4-|"));
     assert_eq!(10, hiker::answer("|X|"));
     assert_eq!(60, hiker::answer("X|X|X"));
+    assert_eq!(270, hiker::answer("X|X|X|X|X|X|X|X|X"));
 }
