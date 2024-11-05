705983565dee66ba88fad97f653de78614a3b2b1
7K21qv <7K21qv@cyber-dojo.org>
2024-11-05 15:57:47 +0000

    30 ran tests, no prediction, got green

--- files/tests/lib_tests.rs
+++ files/tests/lib_tests.rs
@@ -5,6 +5,6 @@ use hiker;
 #[test]
 fn life_the_universe_and_everything() {
     assert_eq!(10, hiker::answer("|12|34|"));
-    assert_eq!(20, hiker::answer("|19|34|"));
+    assert_eq!(20, hiker::answer("|1/|34|"));
     assert_eq!(33, hiker::answer("|1/|3/|42|"));
 }
